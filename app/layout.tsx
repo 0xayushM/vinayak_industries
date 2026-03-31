a04ae4ccac63e4e318da072cf284db15a0c3652a
0xayushm <mangal.ayush.4982@gmail.com>
2026-03-31 12:11:07 +0530

    Add Reb2b tracking script integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,11 @@ export default function RootLayout({
             document.head.appendChild(o)}initApollo();
           `}
         </Script>
+        <Script id="reb2b-tracker" strategy="afterInteractive">
+          {`
+            !function(key) {if (window.reb2b) return;window.reb2b = {loaded: true};var s = document.createElement("script");s.async = true;s.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/" + key + ".js.gz";document.getElementsByTagName("script")[0].parentNode.insertBefore(s, document.getElementsByTagName("script")[0]);}("R6G5YH871V65");
+          `}
+        </Script>
         <VisitorTracker />
         {children}
       </body>
